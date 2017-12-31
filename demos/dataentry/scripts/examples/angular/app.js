432f94047844066e3e977d09161fa004568d133d
Roberto Prevato <roberto.preato@gmail.com>
2017-12-31 12:13:08 +0100

    updates the dataentry demo to latest version of the library

--- demos/dataentry/scripts/examples/angular/app.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  var ug = window.ug = angular.module('ug', ['ug.dataentry'])
-  .run(["$http", function (http) {
-
-  }]);
-
-})();
--- demos/dataentry/scripts/examples/angular/directives/appear.js
+++
@@ -1,9 +0,0 @@
-ug.directive('appear', [function() {
-  return {
-    restrict: 'A',
-    scope: false,
-    link: function (scope, elem, attr, ctrl) {
-      elem.show().css({ "visibility": "visible" });
-    }
-  };
-}]);
